7ce7dbef35d84f7d50bc5603d0062e99acf497c9
Bruno-Pacheco <63880573+Bruno-Pacheco@users.noreply.github.com>
2020-09-24 10:53:10 -0300

    Update mylib.h

--- Código/mylib.h
+++ Código/mylib.h
@@ -27,12 +27,12 @@ parametros_t inicio (void);
 char * getkey (char *);
 char *getdelta (char *);
 
-estados_t friego_off (conf_t);
-estados_t friego_on (conf_t);
+estados_t friego_off (parametros_t);
+estados_t friego_on (parametros_t);
 
 estados_t sensor_agua ();
-estados_t sensor_humedad (conf_t);
-estados_t sensor_lluvia (conf_t);
-estados_t sensor_luz (conf_t);
+estados_t sensor_humedad (parametros_t);
+estados_t sensor_lluvia (parametros_t);
+estados_t sensor_luz (parametros_t);
 
 #endif // MYLIB_H_INCLUDED
