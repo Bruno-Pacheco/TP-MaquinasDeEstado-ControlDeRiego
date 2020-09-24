67fe0c6daf4677c310f6a286bb567d2dd72d3cea
Bruno-Pacheco <63880573+Bruno-Pacheco@users.noreply.github.com>
2020-09-24 10:53:07 -0300

    Update main.c

--- Código/main.c
+++ Código/main.c
@@ -3,17 +3,21 @@
 int main()
 {
     parametros_t config = inicio();
-
     estados_t estado = off;   //estado inicial "riego apagado"
-    estados_t (*pfun[])(parametros_t)={friego_off, friego_on};
+
     printf("****CENTRAL DE RIEGO AUTOMATICO****\n\n");
 
     while (1)
     {
-        estado = (*pfun[estado])(config);
-        if (estado == 1){
-            printf("\n***aspersores prendidos***\n");
-        }else printf("\n***aspersores apagados***\n");
+        switch(estado)
+        {
+            case off:
+                friego_off(config);
+                break;
+            case on:
+                friego_on(config);
+                break;
+        }
     }
     return 0;
 }
